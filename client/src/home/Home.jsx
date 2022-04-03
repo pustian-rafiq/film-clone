087beb0f085758ae1cb5bc42564ac6e792ce9a11
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-04-03 10:12:05 +0600

    project setup

---
+++ client/src/home/Home.jsx
@@ -0,0 +1,12 @@
+import './home.scss'
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+function Home() {
+  return (
+    <div className='home'>
+        <AcUnitIcon/>
+        
+    </div>
+  )
+}
+
+export default Home
