1e369bbf67133e8b6c624ef252a7dfc617a5a5d4
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-12 18:12:15 +0600

    register and login api done

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const User = require("../models/User")
-
+var CryptoJS = require("crypto-js");
 
 // Register the routes
 
@@ -8,7 +8,7 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
     });
     try {
 
@@ -19,4 +19,26 @@ router.post("/register", async (req, res) => {
     }
 })
 
+// Login
+
+router.post("/login", async(req, res) => {
+   // const { email, password } = req.body
+    try {
+        //Check email
+        const user =await User.findOne({ email: req.body.email });
+        !user && res.status(401).json("Wrong username and password!")
+
+        //Check Password
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+        
+        originalPassword !== req.body.password &&  res.status(401).json("Wrong username and password!")
+
+        //Omit password from database doc
+        var {password, ...info} = user._doc
+        res.status(200).json(info)
+    } catch (err) {
+        console.log(err)
+    }
+})
 module.exports = router
